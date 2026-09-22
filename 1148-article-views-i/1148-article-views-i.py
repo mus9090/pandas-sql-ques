772cf3475ffa47f0f49a3c76f0378b680b2590ae
Muskaan Bhatia <167418439+mus9090@users.noreply.github.com>
2026-09-22 15:18:57 +0530

    Time: 293 ms (47.94%), Space: 67.5 MB (45.67%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return views[views["author_id"]== views["viewer_id"]].drop_duplicates("author_id").rename(columns={"author_id":"id"}).sort_values("id")[["id"]]
+    
